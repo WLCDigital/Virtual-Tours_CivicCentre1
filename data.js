95460ef59ddd1e0a654d59513e18555c273b67d0
WLCDigital <86244391+WLCDigital@users.noreply.github.com>
2021-06-21 15:13:41 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,67 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-r0010001_20210621143353",
+      "name": "R0010001_20210621143353",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 3.1113413426399195,
+        "pitch": 0.298648493845004,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -2.502368124348912,
+          "pitch": -0.0838007647716168,
+          "title": "Cupboard",
+          "text": "Full of stuff"
+        },
+        {
+          "yaw": -1.3517689617306985,
+          "pitch": 0.003503186654732815,
+          "title": "Chair",
+          "text": "To sit on"
+        },
+        {
+          "yaw": 1.2185351580904182,
+          "pitch": -0.1624022517973014,
+          "title": "Screen",
+          "text": "To look at"
+        },
+        {
+          "yaw": 2.204710379126694,
+          "pitch": -0.0012576785354170283,
+          "title": "Lockers",
+          "text": "Remember keys"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
